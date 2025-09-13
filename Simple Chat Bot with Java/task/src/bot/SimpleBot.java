394afd0dcdfc0fcb66b87a7112a9f39f8630193f
TomJozz <TomJozz@users.noreply.github.com>
2025-09-14 00:56:30 +0200

    Stage 2 complete

--- Simple Chat Bot with Java/task/src/bot/SimpleBot.java
+++ Simple Chat Bot with Java/task/src/bot/SimpleBot.java
@@ -1,8 +1,30 @@
 package bot;
 
+import java.util.Scanner;
+
 public class SimpleBot {
+    private static final String BOT_NAME = "N00b";
+    private static final int CREATION_YEAR = 2025;
+
     public static void main(String[] args) {
-        System.out.println("Hello! My name is N00b.");
-        System.out.println("I was created in 2025.");
+        try (Scanner scanner = new Scanner(System.in)) {
+            greeting();
+            String name = askForName(scanner);
+            complimentName(name);
+        }
+    }
+
+    private static void greeting() {
+        System.out.printf("Hello! My name is %s.%n", BOT_NAME);
+        System.out.printf("I was created in %d.%n", CREATION_YEAR);
+    }
+
+    private static String askForName(Scanner scanner) {
+        System.out.println("Please, remind me your name.");
+        return scanner.nextLine().trim();
+    }
+
+    private static void complimentName(String name) {
+        System.out.printf("What a great name you have, %s!%n", name);
     }
 }
