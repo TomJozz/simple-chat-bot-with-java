b18ca7d24cfc88fe1076f6873b591a7bc1c443ec
TomJozz <TomJozz@users.noreply.github.com>
2025-09-17 00:55:42 +0200

    Stage 5 complete,project finished.

--- Simple Chat Bot with Java/task/src/bot/SimpleBot.java
+++ Simple Chat Bot with Java/task/src/bot/SimpleBot.java
@@ -3,56 +3,66 @@ package bot;
 import java.util.Scanner;
 
 public class SimpleBot {
+    final static Scanner SCANNER = new Scanner(System.in); // Do not change this line
     private static final String BOT_NAME = "N00b";
-    private static final int CREATION_YEAR = 2025;
+    private static final String CREATION_YEAR = "2025";
 
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            greeting();
-            String name = askForName(scanner);
-            System.out.printf("What a great name you have, %s!%n", name);
-
-            System.out.println("Let me guess your age.");
-            guessAge(scanner);
-
-            System.out.println("Now I will prove to you that I can count to any number you want.");
-            countOutTo(scanner);
-
-            System.out.println("Completed, have a nice day!");
-        }
+        greet(BOT_NAME, CREATION_YEAR);
+        remindName();
+        guessAge();
+        count();
+        test();
+        end();
     }
 
-    private static void countOutTo(Scanner scanner) {
-        int countTo = scanner.nextInt();
-        for (int i = 0; i <= countTo; i++) {
+    static void greet(String assistantName, String birthYear) {
+        System.out.printf("Hello! My name is %s.%n", assistantName);
+        System.out.printf("I was created in %s.%n", birthYear);
+        System.out.println("Please, remind me of your name.");
+    }
+
+    static void remindName() {
+        String name = SCANNER.nextLine().trim();
+        System.out.printf("What a great name you have, %s!%n", name);
+    }
+
+    static void guessAge() {
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+        int rem3 = SCANNER.nextInt();
+        int rem5 = SCANNER.nextInt();
+        int rem7 = SCANNER.nextInt();
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+        System.out.printf("Your age is %d; that's a good time to start programming!%n", age);
+    }
+
+    static void count() {
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+        int num = SCANNER.nextInt();
+        for (int i = 0; i <= num; i++) {
             System.out.printf("%d!%n", i);
         }
     }
 
-    private static void guessAge(Scanner scanner) {
-        RemaindersInput input = getRemainders(scanner);
-        int age = (input.remainder3() * 70 + input.remainder5() * 21 + input.remainder7() * 15) % 105;
-        System.out.printf("Your age is %d; that's a good time to start programming!%n", age);
+    static void test() {
+        System.out.println("Let's test your programming knowledge.");
+        System.out.println("Why do we use methods?");
+        System.out.println("1. To repeat a statement multiple times.");
+        System.out.println("2. To decompose a program into several small subroutines.");
+        System.out.println("3. To determine the execution time of a program.");
+        System.out.println("4. To interrupt the execution of a program.");
+
+        int answer;
+        do {
+            answer = SCANNER.nextInt();
+            if (answer != 2) {
+                System.out.println("Please, try again.");
+            }
+        } while (answer != 2);
     }
 
-    private static RemaindersInput getRemainders(Scanner scanner) {
-        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
-        int remainder3 = scanner.nextInt();
-        int remainder5 = scanner.nextInt();
-        int remainder7 = scanner.nextInt();
-        return new RemaindersInput(remainder3, remainder5, remainder7);
-    }
-
-    private record RemaindersInput(int remainder3, int remainder5, int remainder7) {}
-
-    private static void greeting() {
-        System.out.printf("Hello! My name is %s.%n", BOT_NAME);
-        System.out.printf("I was created in %d.%n", CREATION_YEAR);
-    }
-
-    private static String askForName(Scanner scanner) {
-        System.out.println("Please, remind me your name.");
-        return scanner.nextLine().trim();
+    static void end() {
+        System.out.println("Congratulations, have a nice day!"); // Do not change this text
     }
 }
-
